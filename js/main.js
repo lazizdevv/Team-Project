49739aae35f15aba264671d6395ddf233ec73b45
Lazizbek <oddiybolakay05@gmail.com>
2024-03-31 23:47:28 +0500

    branch to main

--- js/main.js
+++ js/main.js
@@ -2,19 +2,32 @@ let fixMob = document.querySelector(".fix-mob");
 
 let closeBtn = document.querySelector(".close-btn");
 
+let fixPln = document.querySelector(".fix-pln");
+
+
 fixMob.addEventListener("click", ()=>{
     let group = document.querySelector(".group");
     group.classList.toggle("df");
-    // group.style.display = "flex"
     fixMob.classList.toggle("bottom");
 })
 
 closeBtn.addEventListener("click", () => {
   let group = document.querySelector(".group");
-//   group.style.display = "none";
     group.classList.remove("df");
     fixMob.classList.remove("bottom");
-    
+});
 
 
+
+fixPln.addEventListener("click", () => {
+  let group = document.querySelector(".group");
+  group.classList.toggle("df");
+  fixPln.classList.toggle("bottom");
+  
+});
+
+closeBtn.addEventListener("click", () => {
+  let group = document.querySelector(".group");
+  group.classList.remove("df");
+  fixPln.classList.remove("bottom");
 });
